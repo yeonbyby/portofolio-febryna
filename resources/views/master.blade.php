5af4237539198d1d3c5c452e84663ba37efde03c
yeonby <febrynanurulcahyani@gmail.com>
2026-05-16 11:16:59 +0700

    css

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Home - Febryna</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
 
 </head>
 
